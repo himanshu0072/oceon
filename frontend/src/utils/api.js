393d1411f15fb1be0eda82793e4982b8dfcd6d28
himanshu0072 <himanshu2003prajapati@gmail.com>
2026-06-13 02:01:26 +0530

    final

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 console.log("VITE_API_URL =", process.env.VITE_API_URL);
 
-const api = axios.create({ baseURL: process.env.VITE_API_URL });
+const api = axios.create({ baseURL: "https://oceon.onrender.com/api" });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("oceon_token");
