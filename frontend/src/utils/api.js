184a33bb51726468ee253fc3faef9ce97c8d0fd6
himanshu0072 <himanshu2003prajapati@gmail.com>
2026-06-13 00:55:02 +0530

    Initial

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,23 +1,23 @@
-import axios from 'axios';
+import axios from "axios";
 
-const api = axios.create({ baseURL: '/api' });
+const api = axios.create({ baseURL: "import.meta.env.VITE_API_URL/api" });
 
-api.interceptors.request.use(config => {
-  const token = localStorage.getItem('oceon_token');
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("oceon_token");
   if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
 
 api.interceptors.response.use(
-  res => res,
-  err => {
+  (res) => res,
+  (err) => {
     if (err.response?.status === 401) {
-      localStorage.removeItem('oceon_token');
-      localStorage.removeItem('oceon_user');
-      window.location.href = '/login';
+      localStorage.removeItem("oceon_token");
+      localStorage.removeItem("oceon_user");
+      window.location.href = "/login";
     }
     return Promise.reject(err);
-  }
+  },
 );
 
 export default api;
