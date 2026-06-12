49b78d340574504674b4566c0ca64f72363783b1
himanshu0072 <himanshu2003prajapati@gmail.com>
2026-06-13 01:57:03 +0530

    updated

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-console.log("VITE_API_URL =", import.meta.env.VITE_API_URL);
+console.log("VITE_API_URL =", process.env.VITE_API_URL);
 
-const api = axios.create({ baseURL: import.meta.env.VITE_API_URL });
+const api = axios.create({ baseURL: process.env.VITE_API_URL });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("oceon_token");
